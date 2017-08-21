0c271a4fd3bfc67826b954563d5abac320cc318c
Zalak Shah <parikhzalak2503@gmail.com>
2017-08-21 14:51:51 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Challange1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter FileName with Path...");
+            string FileName = Console.ReadLine();
+
+            int Count = 1;
+            string NewFileName, FinalFileName = "";
+
+            int startIndex = FileName.LastIndexOf('\\');
+            string Path = FileName.Substring(0, startIndex + 1);
+            string TempFileName = FileName.Substring(startIndex + 1, (FileName.LastIndexOf('.') - startIndex) - 1);
+            string FileExtension = FileName.Substring(FileName.LastIndexOf('.'));
+
+            NewFileName = TempFileName + " - Copy";
+
+            string[] FileList = Directory.GetFiles(Path);
+
+            //string ExistFileName;
+            foreach (string Fname in FileList)
+            {
+                if (Fname.Contains(NewFileName) && !Fname.Contains(NewFileName + " -"))
+                {
+                    //ExistFileName = Fname;
+                    Count++;
+                }
+            }
+
+            if (Count == 1)
+                FinalFileName = Path + TempFileName + " - Copy" + FileExtension;
+            else
+                FinalFileName = Path + TempFileName + " - Copy (" + Count.ToString() + ")" + FileExtension;
+
+            try
+            {
+                File.Copy(FileName, FinalFileName, false);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("already exists"))
+                {
+
+                }
+            }
+            //}
+        }
+    }
+}
